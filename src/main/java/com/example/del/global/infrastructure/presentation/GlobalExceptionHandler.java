f447872a97900f9c7f7d8ea2d1ef92076e5ffbc2
euneuneun <daeun2597@gmail.com>
2026-04-19 21:53:35 +0900

    chore : global 환경 세팅

---
+++ src/main/java/com/example/del/global/infrastructure/presentation/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.example.del.global.infrastructure.presentation;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
